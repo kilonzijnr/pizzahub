3021fa5485b8bcd4f2178edd28299e8b21ba5e6c
kilonzijnr <vkilonzi05@gmail.com>
2021-08-04 10:12:58 +0300

    added script code to all fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,81 @@
 let size, crust, toppings
-function submitOrder() {
+let numbersOf
+function getInputs() {
     size = document.getElementById("psize").value;
     crust = document.getElementById("ctype").value;
     toppings = document.getElementById("ttype").value;
-    numbersOf = parseInt(document.getElementById("number").value);
+    numbersOf = parseInt(document.getElementById("npizza").value);
     console.log(size);
     console.log(crust);
     console.log(toppings);
     console.log(numbersOf);
 }
+let priceOfSize, priceOfCrust, priceOfToppings, yourTotalPrice
+function totalPrice() {
+    getInputs()
+    if (size == "small") {
+        priceOfSize = 600;
+        console.log(priceOfSize);
+
+    } else if (size == "medium") {
+        priceOfSize = 900;
+        console.log(priceOfSize);
+
+    }
+
+    else if (size == "large") {
+        priceOfSize = 1200;
+        console.log(priceOfSize);
+    }
+
+    if (crust == "hawaiian") {
+        priceOfCrust = 270;
+        console.log(priceOfCrust);
+    }
+
+    else if (crust == "stuffed") {
+        priceOfCrust == 350;
+        console.log(priceOfCrust);
+    }
+
+    else if (crust == "cracker") {
+        priceOfCrust = 250;
+        console.log(priceOfCrust);
+    }
+
+    else if (crust == "thin") {
+        priceOfCrust = 200;
+        console.log(priceOfCrust);
+    }
+
+    if (toppings == "pepperoni") {
+        priceOfToppings = 300;
+        console.log(priceOfToppings);
+    }
+
+    else if (toppings == "sausage") {
+        priceOfToppings == 150;
+        console.log(priceOfToppings);
+    }
+
+    else if (toppings == "bacon") {
+        priceOfToppings = 200;
+        connsole.log(priceOfToppings);
+    }
+
+    else if (toppings == "pineapple") {
+        priceOfToppings = 200;
+        connsole.log(priceOfToppings);
+    }
+
+    yourTotalPrice = (priceOfSize + priceOfCrust + priceOfToppings) * numbersOf;
+    console.log(yourTotalPrice)
+
+    if (yourTotalPrice < 0) {
+        alert("This is incorrect please make an order")
+
+    } else {
+        document.getElementById("output").innerHTML = "Your total price for " + numbersOf + " " + size + " " + crust + " " + toppings + " " + "pizza(s) " + " is Ksh. " + yourTotalPrice;
+    }
+};
+
